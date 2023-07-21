5474520024766fb157af5976e5fcc7283f0fd817
azucenacornejo <cornejoquispeazucena@gmail.com>
2023-07-21 10:04:43 -0500

    v03

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import C01component from './component/C01component';
 import C02contador from './component/C02contador';
 import C03dobleestado from './component/C03dobleestado';
+import C04variable from './component/C04variable';
+import C05operadorTernario from './component/C05operadorTernario';
+import C06matriz from './component/C06matriz';
+import C07matrizOperaciones from './component/C07matrizOperaciones';
 
 function App() {
   return (
@@ -13,6 +17,17 @@ function App() {
       <C02contador />
       <h1>Componente 3</h1>
       <C03dobleestado/>
+      <h1>Componente 4</h1>
+      <C04variable/>
+      <h1>Componente 5</h1>
+      <C05operadorTernario/>
+      <h1>Componente 6</h1>
+      <C06matriz/>
+      <h1>Componente 7</h1>
+      <C07matrizOperaciones/>
+      
+
+      
     </div>
   );
 }
--- src/component/C04variable.js
+++ src/component/C04variable.js
@@ -3,7 +3,7 @@ const variable = (msj) =>{
     return (
         <div>
             <h1>Hola Mundo...lectura a una variable</h1>
-            <h2>Mensaje:{msj.xVariable}</h2>
+            <h2>Mensaje:hola mono chao mono {msj.xVariable}</h2>
         </div>
     );
 }
