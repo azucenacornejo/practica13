6f65e270a646c137a78b31e93bf911c28403ed2c
azucenacornejo <cornejoquispeazucena@gmail.com>
2023-08-04 10:06:40 -0500

    blanca

--- src/App.js
+++ src/App.js
@@ -11,20 +11,13 @@ import C07matrizOperaciones from './component/C07matrizOperaciones';
 function App() {
   return (
     <div className="App">
-      <h1>Componente 1</h1>
-      <C01component  />
-      <h1>Componente 2</h1>
-      <C02contador />
-      <h1>Componente 3</h1>
-      <C03dobleestado/>
-      <h1>Componente 4</h1>
-      <C04variable/>
-      <h1>Componente 5</h1>
-      <C05operadorTernario/>
       <h1>Componente 6</h1>
       <C06matriz/>
-      <h1>Componente 7</h1>
-      <C07matrizOperaciones/>
+      <h1>Componente 4</h1>
+      <C04variable/>
+      <h1>Componente 2</h1>
+      <C02contador />
+    
       
 
       
