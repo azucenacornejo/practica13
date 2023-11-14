e3b4207b639dc02a272b5c70c096b9c6fab33a3d
blanca <cornejoquispeazucena@gmail.com>
2023-11-14 18:55:26 -0500

    examen2023

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import C01component from './component/C01component';
+import C06componente from './component/C06componente';
+import C05contador from './component/C05contador';
+import C04doblecontador from './component/C04doblecontador';
+import C03variable from './component/C03variable';
+import C02operador from './component/C02operador';
+import C01matriz from './component/C01matriz';
 
 function App() {
   return (
-    <div className="App">
-      <C01component/>
-     
+    <div>
+  
+      <h3>HONORIO DELGADO ESPINOZA"</h3>
+      
+      <h3>Blanca Azucena Cornejo Quispe</h3>
     
+      <h3>NOCHE VI SEMESTRE</h3>
+      
       
 
-      
+
     </div>
   );
 }
